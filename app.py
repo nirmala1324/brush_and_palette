54b0c4f914a25123700653cd99e683daffb827bf
Genxyng <103422071+Genxyng@users.noreply.github.com>
2023-12-12 20:16:00 +0700

    update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def sign_in():
             "id": username_receive,
             "exp": datetime.utcnow() + timedelta(seconds=60 * 60 * 24),
         }
-        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256").decode("utf-8")
+        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
         return jsonify(
             {
