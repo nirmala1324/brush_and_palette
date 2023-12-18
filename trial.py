d88d7e3b1f00fe80b76802b8bce7f24b79fbcfb0
Genxyng <103422071+Genxyng@users.noreply.github.com>
2023-12-18 14:58:23 +0700

    update about-us

--- trial.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_path = "./static/admin/artwork/AW_ajbn.jpg"
-
-try:
-    os.remove(file_path)
-    print(f"File {file_path} successfully deleted.")
-except OSError as e:
-    print(f"Error: {file_path} - {e}")
